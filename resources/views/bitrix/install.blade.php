78bfb873fbc53ba3258560b9cdc118ed6b3e6c7f
usrssssx <swiftkey965@gmail.com>
2026-03-12 23:13:26 +0300

    Подключил Bitrix24 JS SDK на странице установки и улучшил завершение установки

--- resources/views/bitrix/install.blade.php
+++ resources/views/bitrix/install.blade.php
@@ -4,14 +4,46 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bitrix24 local app installation</title>
+    <script src="https://api.bitrix24.com/api/v1/"></script>
 </head>
 <body>
 <p>Установка приложения завершается...</p>
 <script>
-    if (window.BX24 && typeof window.BX24.installFinish === 'function') {
-        window.BX24.installFinish();
-    } else {
+    const showFallbackMessage = () => {
         document.body.insertAdjacentHTML('beforeend', '<p>Не удалось завершить установку автоматически. Откройте приложение из меню Bitrix24.</p>');
+    };
+
+    const tryInstallFinish = () => {
+        if (window.BX24 && typeof window.BX24.installFinish === 'function') {
+            window.BX24.installFinish();
+            return true;
+        }
+
+        if (window.parent && window.parent.BX24 && typeof window.parent.BX24.installFinish === 'function') {
+            window.parent.BX24.installFinish();
+            return true;
+        }
+
+        if (window.top && window.top.BX24 && typeof window.top.BX24.installFinish === 'function') {
+            window.top.BX24.installFinish();
+            return true;
+        }
+
+        return false;
+    };
+
+    if (window.BX24 && typeof window.BX24.init === 'function') {
+        window.BX24.init(() => {
+            if (!tryInstallFinish()) {
+                showFallbackMessage();
+            }
+        });
+    } else if (!tryInstallFinish()) {
+        setTimeout(() => {
+            if (!tryInstallFinish()) {
+                showFallbackMessage();
+            }
+        }, 1200);
     }
 </script>
 </body>
